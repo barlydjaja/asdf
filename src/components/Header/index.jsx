9228d8883c79fbc147371c2e0c0a142f1a2a1b37
barlydjaja <barlydjaja@gmail.com>
2021-07-02 22:12:03 +0700

    add header animation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import wiketLogo from "../../assets/images/svg/wiket-logo.svg";
 import "./index.scss";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(window.pageYOffset > 150);
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const navigationAnimate = {
+    hidden: {
+      opacity: screenSize > 768 ? 0 : 1,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: screenSize > 768 ? 1 : 0,
+      },
+    },
+  };
 
   useEffect(() => {
     window.onscroll = () => {
       setIsScrolled(window.pageYOffset > 150);
       console.log(window.pageYOffset);
     };
+    window.onresize = () => {
+      setScreenSize(window.innerWidth);
+    };
   }, []);
 
   return (
-    <div
+    <motion.div
+      variants={navigationAnimate}
+      initial="hidden"
+      animate="show"
       className={"navigation flex-justify-between flex-align-center"}
       style={{
         backgroundColor: isScrolled ? "#ffffff" : "",
@@ -41,7 +61,7 @@ const Header = () => {
           <div className="line3"></div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
